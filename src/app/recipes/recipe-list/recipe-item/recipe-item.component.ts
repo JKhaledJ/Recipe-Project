4ce0d81c6265e90c866a746e92a5f345fccd86d5
Khaled Jami <mkjami.1998@gmail.com>
2020-07-16 23:08:06 +0530

    added dynamic item id

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -7,6 +7,7 @@ import { Recipe } from '../../recipe.model';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+@Input() id: number;
   @Input() recipe:Recipe;
 
   ngOnInit(): void {
